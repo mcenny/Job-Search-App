47658856bc176a66cb4d15be84e8d05330c515be
mcenny <dev.mcenny@gmail.com>
2023-07-17 09:45:53 +0100

    started creating the popular jobs component

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,54 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
+import { useRouter } from 'expo-router';
 
-import styles from './popularjobs.style'
+import styles from './popularjobs.style';
+import { SIZES, COLORS } from '../../../constants';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+import useFetch from '../../../hook/useFetch';
 
 const Popularjobs = () => {
-  return (
-    <View>
-      <Text>Popularjobs</Text>
-    </View>
-  )
-}
+  const router = useRouter();
 
-export default Popularjobs
+  const { loading, data, error, refetch } = useFetch({
+    endpoint: 'search',
+    query: {
+      query: 'Python developer in Texas, USA',
+      page: '1',
+      num_pages: '1',
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity onPress={refetch}>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.cardsContainer}>
+        {loading ? (
+          <ActivityIndicator size={'large'} color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong...</Text>
+        ) : (
+          <FlatList
+            data={[1, 2, 3, 4, 5, 6, 7, 8]}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
+            keyExtractor={(item) => item}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs;
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,61 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './welcome.style'
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from 'react-native';
+import { useRouter } from 'expo-router';
+import styles from './welcome.style';
+import { SIZES, icons, images } from '../../../constants';
 
 const Welcome = () => {
+  const router = useRouter();
+  const jobsType = ['fulltime', 'part-time', 'contract'];
+  const [activeJobType, setActiveJobType] = useState('fulltime');
   return (
     <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Phils</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect jobs</Text>
+      </View>
 
-export default Welcome
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            // value=''
+            onChange={() => {}}
+            placeholder='What are you looking for?'
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image source={icons.search} style={styles.searchBtnImage} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobsType}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
