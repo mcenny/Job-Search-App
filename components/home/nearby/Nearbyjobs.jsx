6365a32fdda0065b9f24802b3d96ee9f5d9c203e
mcenny <dev.mcenny@gmail.com>
2023-07-18 08:31:34 +0100

    finished populating NearbyJobs Component

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,14 +1,49 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { useRouter } from 'expo-router';
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native';
 
-import styles from './nearbyjobs.style'
+import styles from './nearbyjobs.style';
+import { COLORS } from '../../../constants';
+import NearbyJobCard from '../../common/cards/nearby/NearbyJobCard';
+import useFetch from '../../../hook/useFetch';
+import { nearbyJobs } from '../../../data';
 
 const Nearbyjobs = () => {
-  return (
-    <View>
-      <Text>Nearbyjobs</Text>
-    </View>
-  )
-}
+  const router = useRouter();
+  // const { data, loading, error } = useFetch('search', {
+  //   query: 'React Native developer',
+  //   num_pages: '1',
+  // });
 
-export default Nearbyjobs
+  const loading = false;
+  const error = false;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Nearby jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {loading ? (
+          <ActivityIndicator size='large' color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          nearbyJobs?.map((job) => (
+            <NearbyJobCard
+              job={job}
+              key={`nearby-job-${job.job_id}`}
+              handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
+            />
+          ))
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Nearbyjobs;
--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -11,18 +11,25 @@ import styles from './popularjobs.style';
 import { SIZES, COLORS } from '../../../constants';
 import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 import useFetch from '../../../hook/useFetch';
+import { popularJobs } from '../../../data';
 
 const Popularjobs = () => {
   const router = useRouter();
 
-  const { loading, data, error, refetch } = useFetch({
-    endpoint: 'search',
-    query: {
-      query: 'Python developer in Texas, USA',
-      page: '1',
-      num_pages: '1',
-    },
-  });
+  // const { loading, data, error, refetch } = useFetch({
+  //   endpoint: 'search',
+  //   query: {
+  //     query: 'React Native Developer',
+  //     page: '1',
+  //     num_pages: '1',
+  //   },
+  // });
+
+  const error = false;
+  const loading = false;
+
+  // console.log(data, ':: data');
+  // const jobs = data.data;
 
   return (
     <View style={styles.container}>
@@ -39,7 +46,7 @@ const Popularjobs = () => {
           <Text>Something went wrong...</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4, 5, 6, 7, 8]}
+            data={popularJobs}
             renderItem={({ item }) => <PopularJobCard item={item} />}
             keyExtractor={(item) => item}
             contentContainerStyle={{ columnGap: SIZES.medium }}
